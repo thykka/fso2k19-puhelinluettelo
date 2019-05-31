a5cfd5278fa2c458652f1fe4c8456b9b5921d00c
Moses Holmström <thykka@gmail.com>
2019-05-31 12:46:23 +0300

    cors

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const API_URL = '/api/persons';
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
